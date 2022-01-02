8150b7a8330fa2bc632055ddf44b1deb7e2c3514
Lyon Till <lyon.till@microsoft.com>
2022-01-02 17:34:15 +0000

    Update commit

--- src/ConfigStore/GlobalUsings.cs
+++ src/ConfigStore/GlobalUsings.cs
@@ -2,7 +2,7 @@ global using System.CommandLine;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using Microsoft.Azure.Cosmos;
-global using Microsoft.ConfigStore.Schemas;
 global using Microsoft.ConfigStore.Database;
 global using Microsoft.ConfigStore.Files;
 global using Microsoft.ConfigStore.Items;
+global using Microsoft.ConfigStore.Schemas;
